455c66b836a42344f1967defe22d43501bd48f7b
KiziridisK <kiziridk@csd.auth.gr>
2022-08-31 18:15:48 +0300

    Density Based method implementation
    Implemented density based method for anomaly detection in time series

--- DensityBasedTechniques.py
+++ DensityBasedTechniques.py
@@ -14,16 +14,64 @@ def get_data():
     return data
 
 
-def calculate_time_diff(a:TimeObj, b:TimeObj):
+def calculate_time_diff(a: TimeObj, b: TimeObj):
 
-    time_diff = abs(a.hour - b.hour)*60 + abs(a.minute - b.minute)
+    time_diff = 0
     if a.hour > b.hour:
-        if(a.minute >)
+        starting = b
+        ending = a
     elif a.hour < b.hour:
+        starting = a
+        ending = b
+    else:
+        time_diff = abs(a.minute - b.minute)
+        return time_diff
+
+    if ending.minute > starting.minute:
+        time_diff = abs(a.hour - b.hour) * 60 + abs(a.minute - b.minute)
+    elif ending.minute < starting.minute:
+        ending.hour -= 1
+        ending.minute += 60
+        time_diff = abs(a.hour - b.hour) * 60 + abs(a.minute - b.minute)
 
     return time_diff
 
+
+def check_r(a, b, r):
+
+    if abs(a - b) < r:
+        return True
+    else:
+        return False
+
+
+def density_based_outlier_detection(r, time_window, t, data_points):
+
+    outliers = []
+
+    for i in range(len(data_points)):
+        entries = []
+        for j in range(len(data_points)):
+            if check_r(data_points[i][0], data_points[j][0], r):
+                if calculate_time_diff(data_points[i][1], data_points[j][1]) < time_window:
+                    entries.append(data_points[j])
+                else:
+                    pass
+            else:
+                pass
+
+        if len(entries) < t:
+            outliers.append([i, data_points[i]])
+        else:
+            pass
+
+    return outliers
+
+
 def main():
+
+    # Setting the start time
+    start_time = time.time()
 
     data = get_data()
     d = array(data)
@@ -38,10 +86,13 @@ def main():
     for i in range(len(data)-1):
         list_of_data_points.append([data[i+1][0], TimeObj(data[i+1][1], data[i+1][2])])
 
-    #print(list_of_data_points[0][1].hour)
-    a = TimeObj(12, 15)
-    b = TimeObj(10, 10)
-    print(calculate_time_diff(a, b))
+    outliers = density_based_outlier_detection(10, 250, 3, list_of_data_points)
+    print(len(outliers))
+    print(outliers)
+
+    # Getting the end time
+    end_time = time.time()
+    print("Time needed for code execution: ", end_time - start_time, " seconds.")
 
 
 if __name__ == '__main__':
